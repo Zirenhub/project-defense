959f2d62b581734d891f5bee02deec9f7a33cc66
Erdinch <erdinchorhan02@gmail.com>
2023-07-27 01:47:13 +0300

    middleware added, reply added

--- server/src/routes/tweetRoute.ts
+++ server/src/routes/tweetRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { create, like } from '../controllers/tweetController';
+import { create, like, reply } from '../controllers/tweetController';
 import { jwtAuth } from '../middleware/jwtAuth';
+import { getTweet } from '../middleware/getTweet';
 
 const router = express.Router();
 
 router.post('/', jwtAuth, create);
-router.post('/:id', jwtAuth); // comment
-router.post('/:id/like', jwtAuth, like);
+router.post('/:id', jwtAuth, getTweet, reply);
+router.post('/:id/like', jwtAuth, getTweet, like);
 
 export default router;
