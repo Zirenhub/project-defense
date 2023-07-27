4d13ce5fef2b15345fda6d25e276101eb07839fa
Erdinch <erdinchorhan02@gmail.com>
2023-07-27 17:01:04 +0300

    route

--- server/src/routes/tweetRoute.ts
+++ server/src/routes/tweetRoute.ts
@@ -1,12 +1,24 @@
 import express from 'express';
-import { create, like, reply } from '../controllers/tweetController';
+import {
+  create,
+  like,
+  reply,
+  get,
+  retweet,
+} from '../controllers/tweetController';
 import { jwtAuth } from '../middleware/jwtAuth';
 import { getTweet } from '../middleware/getTweet';
 
 const router = express.Router();
 
 router.post('/', jwtAuth, create);
+// router.get('/', jwtAuth,) get user's all tweets
+
 router.post('/:id', jwtAuth, getTweet, reply);
+router.get('/:id', getTweet, get);
+
 router.post('/:id/like', jwtAuth, getTweet, like);
+
+router.post('/:id/retweet', jwtAuth, getTweet, retweet);
 
 export default router;
