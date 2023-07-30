d0cec05dec72efa717f623e2e488f17df0153348
Erdinch <erdinchorhan02@gmail.com>
2023-07-31 02:53:51 +0300

    new timline, modal func

--- client/src/app/home/feed/feed.component.ts
+++ client/src/app/home/feed/feed.component.ts
@@ -3,8 +3,12 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { AppState } from 'src/app/state/app.state';
-import { getTimeline } from 'src/app/state/tweets/tweet.actions';
-import { selectAllTweets } from 'src/app/state/tweets/tweet.selectors';
+import {
+  getTimeline,
+  openReplyTweetModal,
+} from 'src/app/state/tweets/tweet.actions';
+import { Timeline } from 'src/app/state/tweets/tweet.reducer';
+import { selectTweetTimline } from 'src/app/state/tweets/tweet.selectors';
 import { Tweet } from 'src/app/types/Tweet';
 
 @Component({
@@ -14,23 +18,32 @@ import { Tweet } from 'src/app/types/Tweet';
 export class FeedComponent implements OnDestroy {
   timeline: Tweet[] = [];
 
-  timeline$: Observable<Tweet[]>;
+  timeline$: Observable<Timeline | null>;
   timelineSub: Subscription;
 
   constructor(private store: Store<AppState>, private router: Router) {
     this.store.dispatch(getTimeline());
-    this.timeline$ = this.store.select(selectAllTweets);
+    this.timeline$ = this.store.select(selectTweetTimline);
 
     this.timelineSub = this.timeline$.subscribe((timeline) => {
-      this.timeline = timeline;
+      if (timeline) {
+        this.timeline = timeline.tweets;
+      }
     });
+  }
+
+  openReplyModal(id: string) {
+    const tweet = this.timeline.find((x) => x._id === id);
+    if (tweet) {
+      this.store.dispatch(openReplyTweetModal({ id: tweet._id }));
+    }
+  }
+
+  openTweet(id: string) {
+    this.router.navigate([`/tweet/${id}`]);
   }
 
   ngOnDestroy(): void {
     this.timelineSub.unsubscribe();
-  }
-
-  openTweet(id: string) {
-    this.router.navigate([`/${id}`]);
   }
 }
