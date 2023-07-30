44d4d04120d773cbc1c99343da3f5d1886d88b80
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 04:38:24 +0300

    timeline

--- server/src/routes/tweetRoute.ts
+++ server/src/routes/tweetRoute.ts
@@ -5,6 +5,7 @@ import {
   reply,
   get,
   retweet,
+  timeline,
 } from '../controllers/tweetController';
 import { jwtAuth } from '../middleware/jwtAuth';
 import { getTweet } from '../middleware/getTweet';
@@ -14,8 +15,10 @@ const router = express.Router();
 router.post('/', jwtAuth, create);
 // router.get('/', jwtAuth,) get user's all tweets
 
+router.get('/timeline', jwtAuth, timeline);
+
 router.post('/:id', jwtAuth, getTweet, reply);
-router.get('/:id', getTweet, get);
+router.get('/:id', jwtAuth, getTweet, get);
 
 router.post('/:id/like', jwtAuth, getTweet, like);
 
