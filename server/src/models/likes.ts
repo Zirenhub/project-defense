26b6e7f607d97720f508854579df1fb5fe90829c
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 14:53:32 +0300

    if is liked, then dislike

--- server/src/models/likes.ts
+++ server/src/models/likes.ts
@@ -17,27 +17,42 @@ const LikeSchema = new Schema<ILike>({
 LikeSchema.statics.likeTweet = async function (
   tweetId: Types.ObjectId,
   userId: string
-): Promise<{ status: 'success' | 'error'; model: ILike }> {
+): Promise<{
+  status: 'success' | 'error';
+  model: ILike;
+  likeOrDislike: 'like' | 'dislike';
+}> {
   let model = await this.findOne({ tweet: tweetId });
+
+  let likeOrDislike: 'like' | 'dislike' = 'like';
 
   if (!model) {
     model = await this.create({ tweet: tweetId, likes: [userId] });
   } else {
-    if (!model.likes.includes(userId)) {
+    if (model.likes.includes(userId)) {
+      model.likes = model.likes.filter(
+        (x: mongoose.Schema.Types.ObjectId) => x.toString() !== userId
+      );
+      await model.save();
+      likeOrDislike = 'dislike';
+    } else {
       model.likes.push(userId);
       await model.save();
     }
-    return { status: 'error', model };
   }
 
-  return { status: 'success', model };
+  return { status: 'success', model, likeOrDislike };
 };
 
 interface ILikeModel extends Model<ILike, {}> {
   likeTweet(
     tweetId: Types.ObjectId,
     userId: string
-  ): Promise<{ status: 'success' | 'error'; model: ILike }>;
+  ): Promise<{
+    status: 'success' | 'error';
+    model: ILike;
+    likeOrDislike: 'like' | 'dislike';
+  }>;
 }
 
 const LikeModel = mongoose.model<ILike, ILikeModel>('Like', LikeSchema);
