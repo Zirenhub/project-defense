5ed19b567c0a199c73c33cbaeff64a4194f394c8
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 04:36:08 +0300

    layout now looks better

--- client/src/app/core/user-sidebar/user-sidebar.component.ts
+++ client/src/app/core/user-sidebar/user-sidebar.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/state/app.state';
+import { selectAuthUser } from 'src/app/state/auth/auth.selectors';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-user-sidebar',
   templateUrl: './user-sidebar.component.html',
 })
-export class UserSidebarComponent {}
+export class UserSidebarComponent {
+  buttons = [
+    'Home',
+    'Explore',
+    'Notifications',
+    'Messages',
+    'Lists',
+    'Bookmarks',
+    'Communities',
+    'Verified',
+    'Profile',
+    'More',
+  ];
+
+  user$: Observable<User | null>;
+
+  constructor(private store: Store<AppState>) {
+    this.user$ = this.store.select(selectAuthUser);
+  }
+}
