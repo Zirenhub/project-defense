220b890e4e69fa4de97a213231a4c86cd1ef07fa
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 14:52:15 +0300

    increase reply replies count on reply to reply (...)

--- server/src/controllers/commentController.ts
+++ server/src/controllers/commentController.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express';
 import CommentModel from '../models/comment';
 import { body } from 'express-validator';
 import { IComment } from '../interfaces/IComment';
-import TweetModel from '../models/tweet';
+import LikeModel from '../models/likes';
+import { getExtraTweetInfo } from './tweetController';
+import mongoose, { Document, HydratedDocument } from 'mongoose';
 
 export const reply = [
   body('content')
@@ -22,7 +24,6 @@ export const reply = [
           message: 'Reply not found',
         });
       }
-
       const { content } = req.body;
 
       const newReply = new CommentModel({
@@ -32,7 +33,15 @@ export const reply = [
         parent: reply._id,
       });
       await newReply.populate('profile tweet');
-      await newReply.save();
+      reply.repliesCount = reply.repliesCount + 1;
+
+      const session = await mongoose.startSession();
+      session.startTransaction();
+
+      await newReply.save({ session });
+      await reply.save({ session });
+
+      await session.commitTransaction();
 
       return res.status(200).json({
         status: 'success',
@@ -51,13 +60,14 @@ export const reply = [
 
 export const get = async (req: Request, res: Response) => {
   try {
+    const userId = res.locals.user._id;
     const reply = res.locals.reply;
     const tweet = res.locals.tweet;
 
     const fetchCommentHierarchy = async (
-      comment: IComment
-    ): Promise<IComment[]> => {
-      const hierarchy: IComment[] = [comment];
+      comment: Document<unknown, {}, IComment> & Omit<any, never>
+    ) => {
+      const hierarchy = [comment];
 
       if (comment.parent) {
         const parent = await CommentModel.findById(comment.parent).populate(
@@ -73,11 +83,53 @@ export const get = async (req: Request, res: Response) => {
       return hierarchy;
     };
 
-    const commentHierarchy = await fetchCommentHierarchy(reply.toObject());
+    const commentHierarchy = await fetchCommentHierarchy(reply);
+
+    const modifiedTweet = await getExtraTweetInfo([tweet], userId);
+    const modifedReplies = await getExtraTweetInfo(commentHierarchy, userId);
 
     return res.status(200).json({
       status: 'success',
-      data: { tweet, replies: commentHierarchy },
+      data: { tweet: modifiedTweet[0], replies: modifedReplies },
+      message: null,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      errors: null,
+      message: err instanceof Error ? err.message : 'unknown',
+    });
+  }
+};
+
+export const like = async (req: Request, res: Response) => {
+  try {
+    const replyId = req.params.id;
+    const reply = await CommentModel.findById(replyId);
+    if (!reply) {
+      return res.status(404).json({
+        status: 'error',
+        errors: null,
+        message: 'Reply not found',
+      });
+    }
+    const response = await LikeModel.likeTweet(reply._id, res.locals.user._id);
+
+    if (response.status === 'success') {
+      reply.likesCount = response.model.likes.length;
+    } else {
+      return res.status(400).json({
+        status: 'error',
+        data: null,
+        message: 'Something went wrong while trying to like',
+      });
+    }
+
+    await reply.save();
+
+    return res.status(200).json({
+      status: 'success',
+      data: { _id: reply._id, likeOrDislike: response.likeOrDislike },
       message: null,
     });
   } catch (err) {
