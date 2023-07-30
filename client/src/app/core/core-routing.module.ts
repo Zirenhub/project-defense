bb2d735c767f902194e2c2b94d948fbd359cbd27
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 04:35:50 +0300

    added dynamic routing page for tweets

--- client/src/app/core/core-routing.module.ts
+++ client/src/app/core/core-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { FeedComponent } from '../home/feed/feed.component';
+import { TweetComponent } from '../tweet/tweet.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: 'home', component: FeedComponent }],
+    children: [
+      { path: 'home', component: FeedComponent },
+      { path: ':id', component: TweetComponent },
+    ],
   },
 ];
 
