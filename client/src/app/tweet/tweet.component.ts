4e350da8e90f9137300dcd12745cc95ec06ee17e
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 14:51:41 +0300

    tweet

--- client/src/app/tweet/tweet.component.ts
+++ client/src/app/tweet/tweet.component.ts
@@ -1,39 +1,69 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Location } from '@angular/common';
 import { Observable, Subscription } from 'rxjs';
 import { AppState } from '../state/app.state';
-import { getTweet } from '../state/tweets/tweet.actions';
+import {
+  getTweet,
+  likeReply,
+  likeTweet,
+  postReply,
+} from '../state/tweets/tweet.actions';
 import { Tweet } from '../types/Tweet';
 import { Reply } from '../types/Reply';
-import { selectReplies, selectTweet } from '../state/tweets/tweet.selectors';
+import {
+  selectTweetReplies,
+  selectTweet,
+} from '../state/tweets/tweet.selectors';
 
 @Component({
-  selector: 'app-tweet',
+  selector: 'app-singular-tweet',
   templateUrl: './tweet.component.html',
 })
 export class TweetComponent implements OnInit, OnDestroy {
+  // sub to error and show error pop up
+
   private routeSub: Subscription;
   tweetId?: string;
 
   tweet$: Observable<Tweet>;
   replies$: Observable<Reply[]>;
 
-  constructor(private route: ActivatedRoute, private store: Store<AppState>) {
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<AppState>,
+    private location: Location
+  ) {
     this.routeSub = this.route.params.subscribe((params) => {
       this.tweetId = params['id'] as string;
     });
 
     this.tweet$ = this.store.select(selectTweet);
-    this.replies$ = this.store.select(selectReplies);
+    this.replies$ = this.store.select(selectTweetReplies);
   }
 
   replyText: string = '';
 
   reply() {
-    if (this.replyText) {
-      console.log('reply');
+    if (this.replyText && this.tweetId) {
+      this.store.dispatch(
+        postReply({ id: this.tweetId, content: this.replyText })
+      );
+      this.replyText = '';
     }
+  }
+
+  like(id: string, type: 'tweet' | 'reply') {
+    if (type === 'tweet') {
+      this.store.dispatch(likeTweet({ id }));
+    } else {
+      this.store.dispatch(likeReply({ id }));
+    }
+  }
+
+  back() {
+    this.location.back();
   }
 
   ngOnInit(): void {
