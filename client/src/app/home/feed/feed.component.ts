04d801d0f72af0735efe847d83a8ae244c07e69f
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 04:36:22 +0300

    we now have feed

--- client/src/app/home/feed/feed.component.ts
+++ client/src/app/home/feed/feed.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { AppState } from 'src/app/state/app.state';
+import { getTimeline } from 'src/app/state/tweets/tweet.actions';
+import { selectAllTweets } from 'src/app/state/tweets/tweet.selectors';
+import { Tweet } from 'src/app/types/Tweet';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
 })
-export class FeedComponent {
+export class FeedComponent implements OnDestroy {
+  timeline: Tweet[] = [];
 
+  timeline$: Observable<Tweet[]>;
+  timelineSub: Subscription;
+
+  constructor(private store: Store<AppState>, private router: Router) {
+    this.store.dispatch(getTimeline());
+    this.timeline$ = this.store.select(selectAllTweets);
+
+    this.timelineSub = this.timeline$.subscribe((timeline) => {
+      this.timeline = timeline;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.timelineSub.unsubscribe();
+  }
+
+  openTweet(id: string) {
+    this.router.navigate([`/${id}`]);
+  }
 }
