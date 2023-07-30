095ff955c0507cfffbd15aa0614a5b3c6d2f5cf7
Erdinch <erdinchorhan02@gmail.com>
2023-07-30 14:53:38 +0300

    like reply route

--- server/src/routes/commentRoute.ts
+++ server/src/routes/commentRoute.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { jwtAuth } from '../middleware/jwtAuth';
-import { reply, get } from '../controllers/commentController';
+import { reply, get, like } from '../controllers/commentController';
 import { getReply } from '../middleware/getReply';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.get('/:id', getReply, get);
 router.post('/:id', jwtAuth, reply);
+router.post('/:id/like', jwtAuth, like);
 
 export default router;
