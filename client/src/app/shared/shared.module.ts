e952e0e2fd2bc93d5bd5f7798a68933b4d2cb835
Erdinch <erdinchorhan02@gmail.com>
2023-07-31 06:04:11 +0300

    update module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,28 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValidationErrorComponent } from './validation-error/validation-error.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
-import { TweetComponent } from './tweet/tweet.component';
 import { SvgReplyComponent } from './svgs/svg-reply/svg-reply.component';
 import { SvgRetweetComponent } from './svgs/svg-retweet/svg-retweet.component';
 import { SvgLikeComponent } from './svgs/svg-like/svg-like.component';
+import { TweetContainerComponent } from './tweet-container/tweet-container.component';
+import { ReplyContainerComponent } from './reply-container/reply-container.component';
+import { OpenTweetContainerComponent } from './open-tweet-container/open-tweet-container.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ValidationErrorComponent,
     PopUpComponent,
-    TweetComponent,
     SvgReplyComponent,
     SvgRetweetComponent,
     SvgLikeComponent,
+    TweetContainerComponent,
+    ReplyContainerComponent,
+    OpenTweetContainerComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [
     ValidationErrorComponent,
     PopUpComponent,
-    TweetComponent,
     SvgReplyComponent,
     SvgRetweetComponent,
     SvgLikeComponent,
+    TweetContainerComponent,
+    ReplyContainerComponent,
+    OpenTweetContainerComponent,
   ],
 })
 export class SharedModule {}
