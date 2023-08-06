669b8ae1ecf27b1208985c97f70a9091076f515f
Erdinch <erdinchorhan02@gmail.com>
2023-08-06 22:21:16 +0300

    added a post input on feed

--- client/src/app/home/feed/feed.component.ts
+++ client/src/app/home/feed/feed.component.ts
@@ -3,30 +3,46 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { AppState } from 'src/app/state/app.state';
-import { getTimeline } from 'src/app/state/tweets/tweet.actions';
-import { Timeline } from 'src/app/state/tweets/tweet.reducer';
-import { selectTweetTimline } from 'src/app/state/tweets/tweet.selectors';
+import { sharedContext } from 'src/app/state/shared/shared.actions';
+import { getTimeline } from 'src/app/state/timeline/timeline.actions';
+import { selectTimelineTweets } from 'src/app/state/timeline/timeline.selectors';
 import { Tweet } from 'src/app/types/Tweet';
+import * as sharedActions from '../../state/shared/shared.actions';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
 })
 export class FeedComponent implements OnDestroy {
-  timeline: Tweet[] = [];
+  postContent: string = '';
 
-  timeline$: Observable<Timeline | null>;
+  timeline: Tweet[] = [];
+  context: sharedContext = sharedContext.Timeline;
+
+  timeline$: Observable<Tweet[] | null>;
   timelineSub: Subscription;
 
   constructor(private store: Store<AppState>, private router: Router) {
     this.store.dispatch(getTimeline());
-    this.timeline$ = this.store.select(selectTweetTimline);
+    this.timeline$ = this.store.select(selectTimelineTweets);
 
     this.timelineSub = this.timeline$.subscribe((timeline) => {
       if (timeline) {
-        this.timeline = timeline.tweets;
+        this.timeline = timeline;
       }
     });
+  }
+
+  handlePost() {
+    if (this.postContent) {
+      this.store.dispatch(
+        sharedActions.postTweet({
+          content: this.postContent,
+          context: this.context,
+        })
+      );
+    }
+    this.postContent = '';
   }
 
   ngOnDestroy(): void {
