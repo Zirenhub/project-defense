6837504131c1e2cfcc5668905711026464e2ad3d
Erdinch <erdinchorhan02@gmail.com>
2023-08-06 22:20:54 +0300

    forms module

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
   exports: [FeedComponent],
 })
 export class HomeModule {}
